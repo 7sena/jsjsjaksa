219606c46255655383d2a9a380ddbbe8e756f3b3
7sena <40254036+7sena@users.noreply.github.com>
2018-10-25 15:18:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -346,6 +346,23 @@ client.on('message', message => {
 client.login(process.env.BOT_TOKEN);
 
 
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
 const client2 = new Discord.Client({disableEveryone: true});
 
 const prefix2 = "2";
@@ -675,6 +692,23 @@ client2.login(process.env.BOT_TOKEN2);
 
 
 
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
 
 const client3 = new Discord.Client({disableEveryone: true});
 
